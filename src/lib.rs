11ff9f01f3562d761de7648976df17fddcd7c229
cprohoda <cgprohoda@gmail.com>
2024-11-17 15:06:06 -0500

    Add wgpu support

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,44 @@ use winit::{
     window::WindowBuilder,
 };
 use log::info;
+#[cfg(target_arch="wasm32")]
+use wasm_bindgen::prelude::*;
+#[cfg(target_arch="wasm32")]
+use console_log;
+#[cfg(target_arch="wasm32")]
+use console_error_panic_hook;
+#[cfg(target_arch="wasm32")]
+use wgpu::web_sys;
 
+#[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
 pub fn run() {
-    env_logger::init();
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+            console_log::init_with_level(log::Level::Warn).expect("Logger initialization failed.");
+        } else {
+            env_logger::init();
+        }
+    }
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    #[cfg(target_arch="wasm32")]
+    {
+        use winit::dpi::PhysicalSize;
+        let _ = window.request_inner_size(PhysicalSize::new(450, 400));
+
+        use winit::platform::web::WindowExtWebSys;
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| {
+                let dst = doc.get_element_by_id("learnwgpu")?;
+                let canvas = web_sys::Element::from(window.canvas()?);
+                dst.append_child(&canvas).ok()?;
+                Some(())
+            })
+            .expect("Couldn't append canvas to document body.");
+    }
 
     event_loop.run(move |event, control_flow| match event {
         Event::WindowEvent {
